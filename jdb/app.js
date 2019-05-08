b513aa545c6714aefa7c4e3fd7555ab297dc57e3
Kadiri Joseph <kadiri.joseph@allianz.com>
2019-05-08 07:54:28 +0700

    updated documentation

--- jdb/app.js
+++ jdb/app.js
@@ -1,346 +1,285 @@
-(function()
-{
+(function() {
 
-	'use strict';
+    'use strict';
+    jeli('jelijs.jDB', {
+            requiredModules: ['jeli']
+        })
+        .element({
+            selector: 'app-root',
+            template: '<j-place selector="nav"> </j-place> <j-place selector=".container"> </j-place>',
+            DI: ["$sessionStorage", 'ElementRef']
+        }, baseCtrFN)
+        .element({
+            selector: 'handle-scroll',
+            DI: ['$window', 'ElementRef']
+        }, handleScrollFn);
 
-jEli
-.jModule('jelijs.jDB',['jEli.web.route'])
-.jConfig(["jDebugProvider",jDbuggerFn])
-.jController('baseCtrl',["$scope","$rootModel","$sessionStorage",baseCtrFN])
-.jElement('handleScroll',['$window',handleScrollFn]);
+    function handleScrollFn($window, elementRef) {
+        this.didInit = function() {
+            // $window.on('scroll', function(e) {
+            //     if (this.scrollY > 510) {
+            //         ele.css('top', '70px');
+            //     } else {
+            //         ele.removeAttr('style');
+            //     }
+            // });
 
-//jDebuggerFn
-function jDbuggerFn(jDebugProvider)
-{
-	jDebugProvider.$disableDebugMode = false;
-	//$templateCache.$set('/ui/app/directive/temp.html','<solo-test></solo-test>');	
-}
+            // jEli.dom('li a', ele)
+            // .bind('click', function() {
+            //     jEli.dom('li', ele).removeClass('active');
+            //     jEli.dom(this).parents().addClass('active');
+            // })
+        };
+    }
 
+    //BaseCtrl Fn
+    function baseCtrFN($sessionStorage, elementRef) {
+        var db = null,
+            self = this,
+            errorHandler = function(o) { console.log(o) },
+            successHandler = function(e) { console.log("Success:", e) },
+            cmdLockState = false,
+            cmdFinishState = false,
+            lastRunQuery = '';
+        this.db = {
+            name: 'test',
+            version: 1
+        };
+        this.activeDb = null;
+        this.searchResult = [];
+        this.newData = {};
+        this.queryString = "";
+        this.queryBuilder = "";
+        this.mode = 'learn';
 
-function handleScrollFn($window)
-{
-	return {
-		$init : function(ele,attr,model)
-		{
-			$window.on('scroll',function(e){
-				if(this.scrollY > 510)
-				{
-					ele.css('top','70px');
-				}else{
-					ele.removeAttr('style');
-				}
-			});
+        function logController() {
+            this.console = null;
+            this.print = function(msg) {
+                if (this.console) {
+                    this.console.value = (this.console.value + '\n ' + msg);
+                    this.console.focus();
+                }
+            };
 
-		jEli.dom('li a',ele)
-			.bind('click',function(){
-				jEli.dom('li',ele).removeClass('active');
-				jEli.dom(this).parents().addClass('active');
-			})
-		}
-	}
-}
+            this.clearScreen = function() {
+                var self = this;
+                setTimeout(function() {
+                    self.console.value = "";
+                }, 100);
+            };
+        }
 
-//BaseCtrl Fn
-function baseCtrFN($scope,$rootModel,$sessionStorage)
-{
-	var db = null,
-		errorHandler = function(o){ console.log(o) },
-		successHandler = function(e){ console.log("Success:",e) },
-		cmdLockState = false,
-		cmdFinishState = false,
-		lastRunQuery = '';
-	$scope.db = {};
-	$scope.activeDb = null;
-	$scope.searchResult = [];
-	$scope.newData = {};
-	$scope.queryString = "";
-	$scope.queryBuilder = "";
-	$scope.mode = 'learn';
+        //command prompt
+        var logController = new logController();
 
-	function logController()
-	{
-		this.console = null;
-		this.print = function(msg)
-		{
-			if(this.console)
-			{
-				this.console.value = this.console.value +'\n '+msg;
-			}
-		};
+        //getServiceHost
+        function getServiceHost(addPath) {
+            var loc = location,
+                domain = loc.protocol + "//" + addPath + '.' + loc.hostname;
 
-		this.clearScreen = function()
-		{
-			var self =this;
-			setTimeout(function(){
-				self.console.value = "";
-			},100);
-		};
-	}
+            return domain;
+        }
 
-	//command prompt
-	var logController = new logController();
+        function connectDB(connectDetails) {
+            var req = new jdb(connectDetails.name, connectDetails.version)
+                .open({
+                    serviceHost: null,
+                    logService: function(msg) {
+                        if (msg) {
+                            setMessage(msg);
+                        }
+                    },
+                    interceptor: function(options, state) {
+                        options.headers.Authorization = "Basic af53a5a2f0c5d27f941df9053b5b553f8a594b9c"; //e4dc9b47d414c923abaf123d0c36c0666f10f456
+                    },
+                    storage: connectDetails.storage
+                });
 
-	//getServiceHost
-	function getServiceHost(addPath)
-	{
-		var loc = location,
-			domain = loc.protocol+"//"+addPath+'.'+loc.hostname;
+            req.onSuccess(function(e) {
+                self.activeDb = true;
+                db = e.result;
+                self.dbName = db.name;
+                self.dbVersion = db.version;
+                self.tables = db.info();
+                window.db = db;
+                setTimeout(function() {
+                    logController.console = document.querySelector('textarea');
+                    if (!self.tables.length) {
+                        logController.console.value = ('create -members -[{id:{type:"INT",AUTO_INCREMENT:true}, firstName:{type:"VARCHAR"}, lastName:{type:"VARCHAR"}}]');
+                        logController.console.focus();
+                    }
 
-		return domain;
-	}
-
-	function connectDB(connectDetails)
-	{
-		var req = new jEli.$jDB(connectDetails.name,connectDetails.version)
-					.open({
-							serviceHost:null,
-							logService : function(msg)
-							{
-								if(msg)
-								{
-									setMessage(msg);
-								}
-							},
-							interceptor : function(options,state){
-							},
-							storage:'sqlite'
-						});
-
-			req.onSuccess(function(e)
-			{
-				$scope.activeDb = true;
-				db = e.result;
-				$scope.dbName = db.name;
-				$scope.dbVersion = db.version;
-				$scope.tables = db.info();
-
-				if(!Object.keys($scope.tables).length){
-					setTimeout(function(){
-						jEli.dom('textarea')
-						.val('create -members -[{id:{type:"INT",AUTO_INCREMENT:true}, firstName:{type:"VARCHAR"}, lastName:{type:"VARCHAR"}}]')
-						.focus();
-
-					},500);
-				}
-				//sessionStorage
-				$sessionStorage.setItem("activeSession",JSON.stringify(connectDetails));
-			});
+                }, 100);
+                //sessionStorage
+                $sessionStorage.setItem("activeSession", JSON.stringify(connectDetails));
+            });
 
 
-	}
+        }
 
-	//check active session
-	if($sessionStorage.getItem('activeSession'))
-	{
-		var connection = JSON.parse($sessionStorage.getItem('activeSession') );
-		connectDB(connection);
-	}
+        this.didInit = function() {
+            //check active session
+            if ($sessionStorage.getItem('activeSession')) {
+                var connection = JSON.parse($sessionStorage.getItem('activeSession'));
+                connectDB(connection);
+            }
+        }
 
-	$scope.$disconnect = function()
-	{
-		//disconnect from DB
-		$sessionStorage.removeItem('activeSession');
-		$scope.activeDb = !1;
-	};
+        this.$disconnect = function() {
+            //disconnect from DB
+            $sessionStorage.removeItem('activeSession');
+            this.activeDb = !1;
+            db.close(true);
+        };
 
-	//synchronize the table
-	$scope.syncTable = function(tblName)
-	{
-		db
-		.synchronize()
-		.Entity(db.name)
-		.configSync({
-			serviceHost:'http://ws.jdb.bezynet.com',
-			logService : function(msg)
-			{
-				setMessage(msg);
-			}
-		})
-		.processEntity();
-	};
+        //synchronize the table
+        this.syncTable = function(tblName) {
+            db
+                .synchronize()
+                .Entity(db.name)
+                .configSync({
+                    serviceHost: 'http://ws.jdb.bezynet.com',
+                    logService: function(msg) {
+                        setMessage(msg);
+                    }
+                })
+                .processEntity();
+        };
 
 
-	//create
-	$scope.$create = function()
-	{
-		if($scope.db.name && $scope.db.version)
-		{
-			connectDB($scope.db);
-		}
-	};
-	//DB Function binding test
-	$scope.searchTable = function($table,queryString)
-	{
-		if(queryString)
-		{
-			$scope.queryBuilder = "SELECT:"+queryString+" FROM:"+$table;
-			$scope.$consume(function(){
-			  	$scope.runQuery();
-			});
-		}
-	};
+        //create
+        this.$create = function() {
+            if (this.db.name && this.db.version) {
+                connectDB(this.db);
+            }
+        };
+        //DB Function binding test
+        this.searchTable = function($table, queryString) {
+            if (queryString) {
+                this.queryBuilder = "SELECT:" + queryString + " FROM:" + $table;
+                this.runQuery();
+            }
+        };
 
-	function getTextAreaLine(myString)
-	{
-		return myString.substring(myString.lastIndexOf('\n') + 1, myString.length);
-	}
+        function getTextAreaLine(myString) {
+            return myString.substring(myString.lastIndexOf('\n') + 1, myString.length);
+        }
 
-	$scope.queryMessage = "";
-	$scope.commandPrompt = function($ev)
-	{
-		if($ev.keyCode === 13)
-		{
-			var query = getTextAreaLine($ev.currentTarget.value);
-			$ev.preventDefault();
-			//run query
-			$scope.runQuery(query);
-			logController.console = $ev.currentTarget;
-			cmdLockState = true;
-			lastRunQuery = query;
-		}
-	};
+        this.queryMessage = "";
+        this.commandPrompt = function($ev) {
+            if ($ev.keyCode === 13) {
+                var query = getTextAreaLine($ev.currentTarget.value);
+                $ev.preventDefault();
+                //run query
+                this.runQuery(query);
+                cmdLockState = true;
+                lastRunQuery = query;
+            }
+        };
 
-	function setMessage(msg)
-	{
-		setTimeout(function(){
-			logController.print(msg);
-		},100);
-	}
+        function setMessage(msg) {
+            setTimeout(function() {
+                logController.print(msg);
+            }, 100);
+        }
 
-	function setCMDState()
-	{
-		cmdLockState = false;
-		setMessage('\n');
-	}
+        function setCMDState() {
+            cmdLockState = false;
+            setMessage('\n');
+        }
 
-	function queryHandler()
-	{
-		this.onSuccess = function(ret)
-		{
-			switch(ret.state)
-			{
-				case("insert"):
-				case("delete"):
-				case("update"):
-				case("create"):
-				case("truncate"):
-				case("drop"):
-				case("alter"):
-				case("sync"):
-				case("import"):
-					if( ret.result.message )
-					{
-						$scope.tables = db.info();
-						setMessage( ret.result.message );
-					}
-				break;
-				case("select"):
-					setMessage( ret.jDBNumRows() + " record(s) found");
-					$scope.searchResult = ret.getResult();
-				break;
-				case("help"):
-					for(var i in ret.result.message)
-					{
-						setMessage(ret.result.message[i]);
-					}
-				break;
-				case('cls'):
-					logController.clearScreen();
-				break;
-				default:
-					setMessage( ret.result.message );
-					$scope.searchResult = [];
-				break;
+        function queryHandler() {
+            this.onSuccess = function(ret) {
+                switch (ret.state) {
+                    case ("insert"):
+                    case ("delete"):
+                    case ("update"):
+                    case ("create"):
+                    case ("truncate"):
+                    case ("drop"):
+                    case ("alter"):
+                    case ("sync"):
+                    case ("import"):
+                        if (ret.result.message) {
+                            self.tables = db.info();
+                            setMessage(ret.result.message);
+                        }
+                        break;
+                    case ("select"):
+                        setMessage(ret.jDBNumRows() + " record(s) found");
+                        self.searchResult = ret.getResult();
+                        break;
+                    case ("help"):
+                        for (var i in ret.result.message) {
+                            setMessage(ret.result.message[i]);
+                        }
+                        break;
+                    case ('cls'):
+                        logController.clearScreen();
+                        break;
+                    default:
+                        setMessage(ret.result.message);
+                        self.searchResult = [];
+                        break;
 
-			}
-			
-			setCMDState();
-		};
+                }
 
-		this.onError = function(ret)
-		{
-			if(ret.message)
-			{
-				setMessage(ret.message);
-			}
+                setCMDState();
+                //this.$consume();
+            };
 
-			setCMDState();			
-		};
-	}
+            this.onError = function(ret) {
+                if (ret.message) {
+                    setMessage(ret.message);
+                }
 
-	$scope.runQuery = function(query,set)
-	{
-		if(query && !jEli.$isEqual(lastRunQuery,query))
-		{
-			if(set)
-			{
-				setMessage(query);
-			}
+                setCMDState();
+            };
+        }
 
-			setMessage('Performing Task...');
-			db.jQl(query, new queryHandler());
-		}
-	};
+        this.runQuery = function(query, set) {
+            if (query) {
+                if (set) {
+                    setMessage(query);
+                }
 
-	$scope.importFile = function()
-	{
-		console.log('importing file');
-	}
+                setMessage('Performing Task...');
+                db.jQl(query, new queryHandler());
+            }
+        };
 
-	$scope.addUserTable = function(table,type)
-	{
-		var formData = type[table];
+        this.importFile = function() {
+            console.log('importing file');
+        }
 
-		if(formData)
-		{
-			if(formData.course)
-			{
-				formData.course = Number(formData.course);
-			}
+        this.addDataTable = function(table, type) {
+            var formData = type[table];
+            if (formData) {
+                //insert the info using jDB Query
+                this.runQuery("-INSERT -" + JSON.stringify([formData]) + " -" + table, 1);
+            }
+        };
 
-			if(formData.id)
-			{
-				formData.id = Number(formData.id);
-			}
+        this.truncateTable = function(tblName) {
+            if (tblName) {
+                this.runQuery("-TRUNCATE -" + tblName + " -no", 1);
+            }
+        };
 
-			//insert the info using jDB Query
-			$scope.$consume(function(){
-			  	$scope.runQuery("-INSERT -"+JSON.stringify([formData]) +" -"+table,1);
-			});
-		}
-	};
+        this.setMode = function(mode) {
+            this.mode = mode;
+        };
 
-	$scope.truncateTable = function(tblName)
-	{
-		if(tblName)
-		{
-			$scope.$consume(function()
-			{
-		  		$scope.runQuery("-TRUNCATE -"+tblName+" -no",1);
-		  	});
-		}
-	};
+        this.getMode = function(mode) {
+            return this.mode === mode;
+        };
 
-	$scope.setMode = function(mode)
-	{
-		$scope.mode = mode;
-	};
+        this.dropTable = function(tblName) {
+            if (tblName) {
+                this.runQuery("-DROP -table -" + tblName + " -no", 1);
+            }
+        }
 
-	$scope.getMode = function(mode)
-	{
-		return $scope.mode === mode;
-	};
-
-	$scope.dropTable = function(tblName)
-	{
-		if(tblName)
-		{
-		  $scope.$consume(function(){
-		  	$scope.runQuery("-DROP -table -"+tblName+" -no",1);
-		  });
-		}
-	}
-
-}
-
+    }
+    jeli.app.bootstrapWith('jelijs.jDB', 'app-root');
 })();
